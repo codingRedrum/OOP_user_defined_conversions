4a230086e6c5f57157a484845cdf922609c7de58
Adrian <adrianjuszczak@icloud.com>
2020-12-20 22:53:50 +0100

    Initial Commit

---
+++ 25.1 konwersje def przez usera/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  25.1 konwersje def przez usera
+//
+//  Created by Adrian on 20/12/2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
